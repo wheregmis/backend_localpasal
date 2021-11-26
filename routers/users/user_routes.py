2dd52efa6e89e0af7a4259901e9bc997d5a761d6
Sabin Regmi <xawbeenregmi@gmail.com>
2021-11-26 12:33:36 +0545

    Updated UserModel CICD Testing

--- routers/users/user_routes.py
+++ routers/users/user_routes.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Depends
+from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import HTTPException
 from fastapi_jwt_auth.auth_jwt import AuthJWT
 from starlette import status
-from models.users import User 
-from configs.database import mongodatabase 
+from models.users import User, infoFromUserModel, userFromUserModel 
+from configs.database import mongodatabase
+from schemas.schemas import Login 
 from schemas.users_schemas import serializeDict, serializeList
 from bson import ObjectId
 from routers.authentication.hashing import Hash
@@ -37,14 +39,15 @@ async def find_one_user(id, Authorize:AuthJWT=Depends()):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid Token"
         )
-    return serializeDict(mongodatabase.local.user.find_one({"_id":ObjectId(id)}))
+    return serializeDict(mongodatabase.user.find_one({"_id":ObjectId(id)}))
 
 @user.post('/')
 async def create_user(user: User):
     # need to pass new_user to encrypt password however having issue with bycrpt
-    new_user = User(name=user.name,email=user.email,password=Hash.bcrypt(user.password))
+    new_user:User = userFromUserModel(user)
+    print(new_user)
     mongodatabase.user.insert_one(dict(new_user))
-    return serializeList(mongodatabase.user.find())
+    return jsonable_encoder({"user":infoFromUserModel(new_user)})
 
 @user.put('/{id}')
 async def update_user(id,user: User, Authorize:AuthJWT=Depends()):
